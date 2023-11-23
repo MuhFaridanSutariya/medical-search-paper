2d2399514b0d28c629d6b9fc4cab83bd03c4b785
FARIDAN <suripgentayangan@gmail.com>
2023-11-24 07:21:16 +0800

    first init

---
+++ app.py
@@ -0,0 +1,70 @@
+import os
+import random
+import string
+
+# from apikey import openapikey, serpapikey
+from langchain.llms import OpenAI
+from pdfloader import loadPDF
+from pdfloader import queryPDF
+
+# For utilizing agent search tools
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+
+# For chaining responses
+from langchain import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain 
+from langchain.memory import ConversationBufferMemory
+
+# For frontend user interfacing with app
+import streamlit as st
+from streamlit_chat import message
+
+openapikey = ""
+serpapikey = ""
+
+@st.cache_resource
+def initialize():
+    os.environ['OPENAI_API_KEY'] = openapikey
+    os.environ['SERPAPI_API_KEY'] = serpapikey    
+
+    # temprature 0.0 for consistency response for patients.
+    llm = OpenAI(temperature = 0.0)
+
+    # Streamlit is breaking the memory usage because it keeps re-declaring the conversation buffer memory
+    tool_names =  ["arxiv", "serpapi"]
+    tools = load_tools(tool_names)
+    agent_memory = ConversationBufferMemory(memory_key="chat_history")
+    agent = initialize_agent(tools, llm, agent="conversational-react-description", verbose=True, memory=agent_memory)
+
+    return agent
+
+def main():
+    agent = initialize()
+    # Display the page title and the text box for the user to ask the question
+    st.title('🦜 Search and query academic medical papers ')
+    prompt = st.text_input(" What medical topic would you like to know about? ")      
+
+    if prompt:
+        # save the chat history 
+        if 'answer' not in st.session_state:
+            st.session_state['answer'] = []
+
+        if 'question' not in st.session_state:
+            st.session_state['question'] = [] 
+
+        response = agent.run(prompt)
+
+        st.session_state.question.insert(0,prompt )
+        st.session_state.answer.insert(0,response )   
+
+        # Display the chat history
+        for i in range(len( st.session_state.question)):        
+            questionKey = ''.join(random.choice(string.ascii_letters) for i in range(10))
+            answerKey = ''.join(random.choice(string.ascii_letters) for i in range(10))
+            
+            message(st.session_state['question'][i], is_user=True, key= questionKey)
+            message(st.session_state['answer'][i], is_user=False, key= answerKey)
+
+if __name__ == '__main__':
+    main()
---
+++ pdfloader.py
@@ -0,0 +1,35 @@
+#import dependencies
+import os
+
+from langchain.document_loaders import OnlinePDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+
+def loadPDF(url):
+    # TODO: Temporarily load a PDF from some archive, 
+    # need to change this to the pdf that is retrieved from pubmed API
+    # loader = OnlinePDFLoader("https://arxiv.org/pdf/2302.03803.pdf")
+    loader = OnlinePDFLoader(url)
+    pdf_data = loader.load()
+
+    # Split up the document to avoid token threshold
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 2000,
+        chunk_overlap = 200,
+    )
+
+    # Create OpenAIEmbeddings and FAISS objects. Vectorize the chunks created above and save.
+    documents = text_splitter.split_documents(pdf_data)
+    embeddings = OpenAIEmbeddings()
+    docsearch = FAISS.from_documents(documents, embeddings)
+
+    return docsearch
+
+def queryPDF(query, docsearch):
+    chain = load_qa_chain(OpenAI(), chain_type="stuff")
+    # pass some query to the API to interact with the PDF
+    docs = docsearch.similarity_search(query)
+    chain.run(input_documents=docs, question=query)
